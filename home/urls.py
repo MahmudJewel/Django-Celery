3565e1f70dfb7d1388e61493c6527690ea5e63d3
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-12-29 10:51:41 -0500

    success page added

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 from .views import home
 
 urlpatterns = [
     path('', home, name='home'),
+    path('success-mail', TemplateView.as_view(template_name='feedback/success_mail.html'), name='success_mail'),
 ] 
